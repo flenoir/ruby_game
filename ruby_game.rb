71dfa23fd033d9c5e29dde45404d3ad1c1786253
fabien lenoir <flenoir@gmail.com>
2014-07-08 17:15:07 +0200

    creation de classe

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,6 +1,46 @@
-for j in 1..5
-	for i in 1..5
-	print("x ") 
+
+class Player                     #création de la class player
+	def initialize(x, y) 		 #initialiation de la classe
+		@coordx = x
+		@coordy = y
 	end
- puts("x ")
+
+	def x						#création des accesseurs de la classe
+		return @coordx
+	end
+
+	def y
+		return @coordy			#création des accesseurs de la classe
+	end
 end
+
+player = Player.new(4,6)		#création de l'occurence de la classe
+
+
+
+
+
+def draw (line = 5, row = 5, player)
+
+		for j in 1..line
+
+			for i in 1..row
+
+				 if j == player.x && i == player.y
+					print("P ")
+		  		else
+
+					print("x ") 
+	 			end
+			end
+
+	 	puts
+
+		end
+
+end
+
+draw 10, 8, player
+
+
+
